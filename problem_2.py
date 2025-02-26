8844140e2a5258bd36305777c54c80c286775ebd
james-dewes <james-dewes@users.noreply.github.com>
2025-02-26 12:57:53 +0000

    Improves and updates my answer to problem 2
    Cleans up the code for problem 2 and updates it for python 3

--- problem_2.py
+++ problem_2.py
@@ -1,14 +1,24 @@
-f1 = 1
-f2 = 2
-f3 = 0
-total = 2
+#!/usr/bin/env python3
 
-while f3 < 3999999:
-	f3 = f1 + f2
-	f1 = f2
-	f2 = f3
-	if f3 % 2 == 0:
-		total += f3
+def even_fibonacci_values(limit: int)->int:
+	"""
+	Returns the sum of even Fibonacci sequence numbers
+	below the limit.
+
+	Args: 
+		limit (int): Non negative integer.
+	"""
+	f1 = 0 
+	f2 = 1
+	total = 0
+
+	while f2 < limit:
+		if f2 % 2 == 0:
+			total += f2
+		f1, f2 = f2, (f1 + f2)
+	return total
 	
-print total
+
+if __name__ == '__main__':
+	print(even_fibonacci_values(4000000))
 
