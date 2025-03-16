6ace9116dc85b10d49b3fd7516926ac34f49c7c2
james-dewes <james-dewes@users.noreply.github.com>
2025-03-16 23:55:36 +0000

    Improves and updates my answer to problem 10
    Cleans up the code for problem 10 and updates it for python 3

--- problem_10.py
+++ problem_10.py
@@ -1,15 +1,25 @@
-def is_prime(number):
+#!/usr/bin/env python3
+
+def is_prime(number: int)-> bool:
+	"""
+	Returns True if the number is prime
+	"""
+	result = True
 	for i in range(2,number):
 		if number % i == 0:
-			return False
+			result = False
+			break
+	return result
+
+def main():
+	total = 2
+	n = 1
+	while n < 2000000:
+		if is_prime(n):
+			total = total + n
+		n+=1
 		
-	return True
-	
-total = 2
-n = 1
-while n < 2000000:
-	if is_prime(n):
-		total = total + n
-	n+=1
-	
-print total
+	print(total)
+
+if __name__ == '__main__':	
+	main()
