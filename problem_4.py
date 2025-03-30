82413b30dce2039e3fa7a52b8a1bfed822b0baba
james-dewes <james-dewes@users.noreply.github.com>
2025-03-30 22:58:04 +0100

    Updates problem 4

--- problem_4.py
+++ problem_4.py
@@ -1,22 +1,40 @@
-#largets palindromic number made with 3 digits
+#!/usr/bin/env python3
+def is_palindrome(string)->bool:
+	"""Test if a string is a palindrome.
+	   
+	   Args:
+	   	string (str): any string.
+		   
+	   Returns:
+	    (bool)  True if the string is a palindrome.
+	   	
+	"""
+	return string == string[::-1]
 
-def is_palindrome(n):
-	return str(n) == str(n)[::-1]
+def palindromic_number(n)->int:
+	"""
+	Find the largest palindromic number made with
+	the product of n digit numbers.
 
-def palindromic_number(places):
-	n1 = int	('9'*places)
+	Args:
+		n (int): The number of digits to search.
+	
+	Returns:
+		(int): The largest palindromic number.
+
+	"""
+	n1 = int('9'*n)
 	n2 = n1
 	palindromes = []
-	while n1 != 1:
-		while n2 != 1:
-			if is_palindrome(n1 * n2):
+	while n1 > 0:
+		while n2 > 0:
+			if is_palindrome(str(n1 * n2)):
 				palindromes.append(n1 * n2)
-
+				break
 			n2-=1
-			
 		n2 = n1
 		n1 -=1
 	return max(palindromes)
 
-
-print palindromic_number(3)
+if __name__=='__main__':
+	print(palindromic_number(3))
